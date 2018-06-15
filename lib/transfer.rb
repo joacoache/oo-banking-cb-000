b12377146457acc637e3a14bf4dbaad8f20f72c0
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-06-15 20:02:05 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,20 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status
+
+  def initialize(sender, receiver)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+  end
+
+  def valid?
+  end
+
+  def execute_transaction
+  end
+
+  def reverse_transfer
+  end
+
+
 end
